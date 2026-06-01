6295af57e2fff18f9b0e5d454b4380a8857ea54b
poliveradiaz-ai <p.oliveradiaz@gmail.com>
2026-06-01 13:57:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ if st.button("🚀 Generar Reporte"):
             )
                 df['Especialidad'] = df['Especialidad'].astype(str).str.strip().replace({
                 'TRAUMATOLOGIA Y ORTOPEDIA ADULTO': 'TRAUMATOLOGIA Y ORTOPEDIA'
-                 })
+            })
             # =========================
             # CONTEXTO (ORIGINAL)
             # =========================
