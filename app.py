6da15e756dea92959549617e26c6102d5dfb68e9
poliveradiaz-ai <p.oliveradiaz@gmail.com>
2026-06-01 11:24:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -123,14 +123,14 @@ if st.button("🚀 Generar Reporte"):
             # =========================
             # ERROR FINAL
             # =========================
-            df['Error_Final'] = (df['Es_control'] - df['Interconsulta_Valida'])
-            df['Error_Final'] = (df['Error_Final'] > 0).astype(int)
+            df['Error_escontrol_menos_Inter'] = (df['Es_control'] - df['Interconsulta_Valida'])
+            df['Error_escontrol_menos_Inter'] = (df['Error_escontrol_menos_Inter'] > 0).astype(int)
 
             # =========================
             # TABLA FUNCIONARIOS (SEGÚN ERROR FINAL)
             # =========================
             tabla_funcionarios = (
-                df[df['Error_Final'] == 1]
+                df[df['Error_escontrol_menos_Inter'] == 1]
                 .groupby(['Especialidad', 'Funcionario'])
                 .size()
                 .reset_index(name='total')
@@ -155,7 +155,7 @@ if st.button("🚀 Generar Reporte"):
             # =========================
             # RESUMEN ESPECIALIDADES
             # =========================
-            df_controles = df[df['Error_Final'] == 1]
+            df_controles = df[df['Error_escontrol_menos_Inter'] == 1]
 
             tabla = (
                 df_controles
