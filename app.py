c1ddcd8e5b6a7b1bea6a6117154f0d40f74db965
poliveradiaz-ai <p.oliveradiaz@gmail.com>
2026-08-26 10:41:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("Sube los archivos base y opcionalmente las plantillas")
 st.markdown("## 📂 Archivos base")
 
 datos_file = st.file_uploader("📄 Datos RCE Especialidades.xlsx", type=["xlsx"])
-lp_file = st.file_uploader("📄 Subir Lista de Espera.xlsx", type=["xlsx"])
+lp_file = st.file_uploader("📄 Lista de Espera.xlsx", type=["xlsx"])
 
 st.divider()
 
